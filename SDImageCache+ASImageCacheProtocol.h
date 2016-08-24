10e64374482af476acb28b0a310e0d4ed1baea4f
MarkeJave <308865427@qq.com>
2016-08-24 16:44:49 +0800

    no message

---
+++ SDImageCache+ASImageCacheProtocol.h
@@ -0,0 +1,14 @@
+//
+//  SDImageCache+ASImageCacheProtocol.h
+//  MVVMReactiveCocoa
+//
+//  Created by leichunfeng on 15/11/29.
+//  Copyright © 2015年 leichunfeng. All rights reserved.
+//
+
+#import <SDWebImage/SDImageCache.h>
+#import <AsyncDisplayKit/ASImageProtocols.h>
+
+@interface SDImageCache (ASImageCacheProtocol) <ASImageCacheProtocol>
+
+@end
---
+++ SDWebImageDownloader+ASImageDownloaderProtocol.h
@@ -0,0 +1,14 @@
+//
+//  SDWebImageDownloader+ASImageDownloaderProtocol.h
+//  MVVMReactiveCocoa
+//
+//  Created by leichunfeng on 15/11/29.
+//  Copyright © 2015年 leichunfeng. All rights reserved.
+//
+
+#import <SDWebImage/SDWebImageDownloader.h>
+#import <AsyncDisplayKit/ASImageProtocols.h>
+
+@interface SDWebImageDownloader (ASImageDownloaderProtocol) <ASImageDownloaderProtocol>
+
+@end
